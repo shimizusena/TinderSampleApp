d1b9054dca1b8a90d92eb2cf3935288e892ed840
sena.shimizu <senarugby0813@gmail.com>
2021-06-21 12:06:02 +0900

    first commit

---
+++ TinderSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderSampleApp
+//
+//  Created by 清水世那 on 2021/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TinderSampleAppTests/TinderSampleAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  TinderSampleAppTests.swift
+//  TinderSampleAppTests
+//
+//  Created by 清水世那 on 2021/06/21.
+//
+
+import XCTest
+@testable import TinderSampleApp
+
+class TinderSampleAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
